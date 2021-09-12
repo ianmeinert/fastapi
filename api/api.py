55eadd629c547fd7d14afc26ec5a7d33318df453
Ian Meinert <marinesct@gmail.com>
2021-09-12 13:24:50 -0500

    implemented API POST method

--- api/api.py
+++ api/api.py
@@ -1,12 +1,13 @@
+import fastapi
 import uvicorn
 from typing import List, Dict, Optional
 from datetime import datetime
 from pydantic import BaseModel
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Request
 from fastapi.middleware.cors import CORSMiddleware
 import json
 
-class todo(BaseModel):
+class Todo(BaseModel):
     userId: int
     id: int
     title: str
@@ -26,41 +27,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-__todos: List[todo] = None
+__todos: List[Todo] = None
 
-async def load_todos() -> List[todo]:    
+async def load_todos() -> List[Todo]:    
     with open("datastore/data_file.json", "r") as read_file:
         data = json.load(read_file)
         return data
 
-async def add_todo(todo:todo) -> str:
-    __todos.append({
-        "userId": todo.userId,
-        "id": todo.id,
-        "completed": todo.completed,
-        "title": todo.title
-    })
-    
-    return 'todo added'    
+async def add_todo(todo) -> str:      
+    with open("datastore/data_file.json", "r+") as write_file:
+        file_data = json.load(write_file)
+        file_data.append(todo)
+        write_file.seek(0)
+        json.dump(file_data, write_file, indent = 4)
+        
     
 @app.get('/',name='home')
 async def index():
     return {"message": "Welcome home"}
 
-@app.post('/todo', name='Create a todo', response_model=todo)
-async def todos_post(todo: todo) -> todo:
-    await add_todo(todo = todo)
-    return todo
+@app.post('/todo', name='Create a todo')
+async def todos_post(req: Request):
+    __todos = await load_todos()
+    todo = await req.json()
+    response = await add_todo(todo)
+    
+    return {
+        "status" : "SUCCESS",
+        "data" : todo
+    }
 
-@app.get("/todo/{item_id}", status_code=200, name='Get a todo', response_model=todo)
+@app.get("/todo/{item_id}", status_code=200, name='Get a todo', response_model=Todo)
 async def fetch_todo(item_id: int = Path(..., title="The ID of the item to get")):
     __todos = await load_todos()
     result = [todo for todo in __todos if todo["id"] == item_id]
     if result:
         return result[0]
 
-@app.get('/todos', name='Get all todos', response_model=List[todo])
-async def todos_get() -> List[todo]:
+@app.get('/todos', name='Get all todos', response_model=List[Todo])
+async def todos_get() -> List[Todo]:
     __todos = await load_todos()
     return  __todos
         
